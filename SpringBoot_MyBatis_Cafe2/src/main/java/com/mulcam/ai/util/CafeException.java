23725ccf365d1ae82b0abc302c39a48fdb058bf3
IceDice <dice7912@naver.com>
2021-03-23 16:42:22 +0900

    MyCafe #3

---
+++ SpringBoot_MyBatis_Cafe2/src/main/java/com/mulcam/ai/util/CafeException.java
@@ -0,0 +1,14 @@
+package com.mulcam.ai.util;
+
+public class CafeException extends Exception { 
+
+public CafeException(String message) { 
+
+super(message); 
+System.out.println(message);
+
+ 
+
+} 
+
+} 
